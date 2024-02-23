8dad5d8d012268b15887f775da26c2ba96c0c743
Dejton <patryk.firlus@gmail.com>
2024-02-23 18:03:12 +0100

    add serverImageUrl method to serverService implementation

--- src/main/java/com/servermanagement/Service/implementation/ServerServiceImplementation.java
+++ src/main/java/com/servermanagement/Service/implementation/ServerServiceImplementation.java
@@ -7,14 +7,18 @@ import com.servermanagement.repo.ServerRepo;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Collection;
+import java.util.Random;
 
 import static com.servermanagement.enumeration.Status.SERVER_DOWN;
 import static com.servermanagement.enumeration.Status.SERVER_UP;
+import static java.lang.Boolean.TRUE;
 
 @RequiredArgsConstructor
 @Service
@@ -22,6 +26,7 @@ import static com.servermanagement.enumeration.Status.SERVER_UP;
 @Slf4j
 public class ServerServiceImplementation implements ServerService {
     private final ServerRepo serverRepo;
+
     @Override
     public Server create(Server server) {
         log.info("Saving new server {}", server.getName());
@@ -41,25 +46,32 @@ public class ServerServiceImplementation implements ServerService {
 
     @Override
     public Collection<Server> list(int limit) {
-        return null;
+        log.info("Fetching all servers");
+        return serverRepo.findAll(PageRequest.of(0, limit)).toList();
     }
 
     @Override
     public Server get(long id) {
-        return null;
+        log.info("Fetching server by id {}", id);
+        return serverRepo.findById(id).get();
     }
 
     @Override
     public Server update(Server server) {
-        return null;
+        log.info("Updating server {}", server.getName());
+        return serverRepo.save(server);
     }
 
     @Override
     public Boolean delete(long id) {
-        return null;
+        log.info("Deleting server {}", id);
+        serverRepo.deleteById(id);
+        return TRUE;
     }
+
     private String serverImageUrl() {
-        return null;
+        String[] imagesNames = {"server1.png", "server2.png", "server3.png", "server4.png"};
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image/" + imagesNames[new Random().nextInt(4)]).toUriString();
     }
 
 }
