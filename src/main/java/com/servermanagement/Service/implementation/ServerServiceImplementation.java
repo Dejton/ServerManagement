599585c3ec6bc10855446bae8628f4ad9dba4636
Dejton <patryk.firlus@gmail.com>
2024-02-23 18:03:12 +0100

    Finishing serverService implementation

--- src/main/java/com/servermanagement/Service/implementation/ServerServiceImplementation.java
+++ src/main/java/com/servermanagement/Service/implementation/ServerServiceImplementation.java
@@ -7,6 +7,7 @@ import com.servermanagement.repo.ServerRepo;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -15,6 +16,7 @@ import java.util.Collection;
 
 import static com.servermanagement.enumeration.Status.SERVER_DOWN;
 import static com.servermanagement.enumeration.Status.SERVER_UP;
+import static java.lang.Boolean.TRUE;
 
 @RequiredArgsConstructor
 @Service
@@ -41,22 +43,27 @@ public class ServerServiceImplementation implements ServerService {
 
     @Override
     public Collection<Server> list(int limit) {
-        return null;
+        log.info("Fetching all servers");
+        return serverRepo.findAll(PageRequest.of(0,limit)).toList();
     }
 
     @Override
     public Server get(long id) {
-        return null;
+        log.info("Fetching server by id {}", id);
+        return serverRepo.findById(id).get();
     }
 
     @Override
     public Server update(Server server) {
-        return null;
+        log.info("Updating server {}", server.getName());
+        return serverRepo.save(server);
     }
 
     @Override
     public Boolean delete(long id) {
-        return null;
+        log.info("Deleting server {}", id);
+        serverRepo.deleteById(id);
+        return TRUE;
     }
     private String serverImageUrl() {
         return null;
