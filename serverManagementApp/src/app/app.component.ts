66e8284e22b3cd8d640d337b6f7fd69b6a4389b9
Dejton <patryk.firlus@gmail.com>
2024-02-26 12:32:36 +0100

    Angular cd.

--- serverManagementApp/src/app/app.component.ts
+++ serverManagementApp/src/app/app.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ServerService } from './service/server.service';
+import { Observable, catchError, map, of, startWith } from 'rxjs';
+import { CustomResponse } from './interface/custon-respones';
+import { AppState } from './interface/app-state';
+import { DataState } from './enum/data-state.enum';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +14,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'serverManagementApp';
+export class AppComponent implements OnInit {
+  appState$: Observable<AppState<CustomResponse>>;
+  constructor(private serverService: ServerService) { }
+
+  ngOnInit(): void {
+    this.appState$ = this.serverService.servers$
+      .pipe(
+        map(response => { return { dataState: DataState.LOADED_STATE, addData: response } }),
+        startWith({ dataState: DataState.LOADING_STATE }),
+        catchError((error: string) => {
+          return of({ dataState: DataState.ERROR_STATE, error: error })
+        }
+        ))
+  };
+
 }
--- serverManagementApp/src/app/app.config.ts
+++ serverManagementApp/src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, NgModule } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+
+NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ]
+})
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
+
 };
